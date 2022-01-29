d6acc6e5f1f8fdb12846fc92da85af5181b57485
kiranKamath <k96white@gmail.com>
2022-01-29 16:18:48 +0530

    new file added

---
+++ 13-lodash-example.js
@@ -0,0 +1,5 @@
+const _ = require("lodash");
+console.log(_.chunk([1, 2, 3, 4, 5, "abc"], 3));
+console.log("Compact method : ", _.compact([0, 1, 2, 3, 4, false]));
+const items = [1, [2, [3, [6]], 5]];
+console.log("Flattened array : ", _.flattenDeep(items));
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const _ = require("lodash");
-console.log(_.chunk([1, 2, 3, 4, 5, "abc"], 3));
-console.log("Compact method : ", _.compact([0, 1, 2, 3, 4, false]));
-const items = [1, [2, [3, [4]], 5]];
-console.log("Flattened array : ", _.flattenDeep(items));
+console.log("process started");
+setTimeout(() => {
+  console.log("This is callback");
+}, 200);
+console.log("this a simple messsage");
+console.log("this a simple messsage");
+console.log("this a simple messsage");
+console.log("this a simple messsage");
