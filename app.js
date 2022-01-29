2bc466cff3d38fba73a456713f7a5573c73d8ab8
kiranKamath <k96white@gmail.com>
2022-01-29 19:21:09 +0530

    folder added

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log("process started");
-setTimeout(() => {
-  console.log("This is callback");
-}, 200);
-console.log("this a simple messsage");
-console.log("this a simple messsage");
-console.log("this a simple messsage");
-console.log("this a simple messsage");
